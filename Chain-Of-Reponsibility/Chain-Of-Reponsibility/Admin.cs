c6379759593f421ae40050001838a9f45ca66d62
Clemens Deutinger <lahmaschtv@gmail.com>
2026-09-25 10:15:50 +0200

    users

---
+++ Chain-Of-Reponsibility/Chain-Of-Reponsibility/Admin.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chain_Of_Reponsibility
+{
+    internal class Admin
+    {
+
+    }
+}
---
+++ Chain-Of-Reponsibility/Chain-Of-Reponsibility/User.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chain_Of_Reponsibility
+{
+    internal class User
+    {
+    }
+}
